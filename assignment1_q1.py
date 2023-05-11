a5a9c16e3cc7971901ab3511d3ed6f895c114a54
Navidz <navid.zarrabi.nz@gmail.com>
2023-05-11 13:33:54 -0400

    Edit comments

--- assignment1_q1.py
+++ assignment1_q1.py
@@ -1,22 +1,18 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.stats import chi2
+import matplotlib.pyplot as plt
+
 
 
 lgnd_list=[]
-#x-axis ranges from 0 to 20 with .001 steps
 x = np.arange(0, 200, 0.001)
 
-
-
 for k in [1,2,3,10,100]:
-    #plot Chi-square distribution with 4 degrees of freedom
+    #plot Chi-square distribution with k degrees of freedom
     plt.plot(x, chi2.pdf(x, df=k))
     
     #Plotting the Results
     lgnd_list.append("mu=%.2f & sd=%.2f" %(k,(2*k)))
-    #print(lgnd_list)
-
     plt.xlabel('Data points')
     plt.ylabel('Probability Density')
     plt.xlim(0,120)
